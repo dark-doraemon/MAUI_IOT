761bbd1b15f76b1534d4b435e9c8d2cdaaa8b253
Vuducanh2923 <Vuducanh2923@gmail.com>
2024-09-26 00:04:35 +0700

    updateQR

--- Views/PickerView/EditDevice.xaml.cs
+++ Views/PickerView/EditDevice.xaml.cs
@@ -4,19 +4,39 @@ using MAUI_IOT.ViewModels;
 using System.Diagnostics;
 using System.Collections.ObjectModel;
 
-namespace MAUI_IOT.Views.PickerView;
-
-public partial class EditDevice : BasePopupPage
+namespace MAUI_IOT.Views.PickerView
 {
-    //private AnalyzePopup _analyzePopup;
-    public EditDevice(LessonnViewModel viewModel)
+    [QueryProperty(nameof(QrCodeResult), "Thiết bị phát hiện ")]
+    public partial class EditDevice : BasePopupPage
     {
-        InitializeComponent();
-        BindingContext = viewModel;
-        btnSelectDevice.ViewModel = viewModel;
-    }
+        //private AnalyzePopup _analyzePopup;
+        public EditDevice(LessonnViewModel viewModel)
+        {
+            InitializeComponent();
+            BindingContext = viewModel;
+            btnSelectDevice.ViewModel = viewModel;
+        }
         private async void Button_Clicked(object sender, EventArgs e)
-    {
-        await PopupAction.ClosePopup();
+        {
+            await PopupAction.ClosePopup();
+        }
+        public string QrCodeResult
+        {
+            set
+            {
+                CounterBtn.Text = $"Scanner result :{value}";
+            }
+        }
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+
+            SemanticScreenReader.Announce(CounterBtn.Text);
+        }
+        private async void ScanButtonClick(object sender, EventArgs e)
+        {
+            Routing.RegisterRoute(nameof(ScanPage), typeof(ScanPage));
+
+            await Shell.Current.GoToAsync(nameof(ScanPage));
+        }
     }
 }
---
+++ Views/PickerView/ScanPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using MauiPopup;
+
+namespace MAUI_IOT.Views.PickerView;
+
+public partial class ScanPage : ContentPage
+{
+	public ScanPage()
+	{
+		InitializeComponent();
+        barCodereader.Options = new ZXing.Net.Maui.BarcodeReaderOptions
+        {
+            Formats = ZXing.Net.Maui.BarcodeFormat.QrCode
+        };
+    }
+    private void barCodereader_BarcodesDetected(object sender, ZXing.Net.Maui.BarcodeDetectionEventArgs e)
+    {
+        barCodereader.IsDetecting = false;
+
+        if (e.Results.Any())
+        {
+            var result = e.Results.FirstOrDefault();
+            Dispatcher.Dispatch(async () =>
+            {
+                var navigationParam = new Dictionary<string, object>()
+                {
+                    { "Thiết bị phát hiện ", result.Value }
+                };
+                await Shell.Current.GoToAsync("..", navigationParam);
+            });
+        }
+    }
+}
